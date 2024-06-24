d1cf3e285e350bf8b5faf39ae695305022b6b960
mehdimej1997 <mejjad.el.mehdi@gmail.com>
2024-06-24 12:55:43 +0100

    config

--- web/src/providers/ReactQueryProvider.tsx
+++ web/src/providers/ReactQueryProvider.tsx
@@ -3,12 +3,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 
-const Providers = ({ children }: { children: React.ReactNode }) => {
+export const ReactQueryProvider = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
 }
-
-export default Providers
